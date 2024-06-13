02418bd7187706c453aee53f6597bd3f4c8af8c9
Anderson Suga <anderson.suga@gmail.com>
2024-06-12 21:08:39 -0300

    Criptografia de senhas

--- app.py
+++ app.py
@@ -9,14 +9,9 @@ from flask_login import (
     logout_user,
     login_required,
 )
+import bcrypt
 
 app = Flask(__name__)
-"""
-app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "mysql+pymysql:///root:admin123@localhost:3361/flask-crud"
-)
-"""
 app.config.from_object(Config)
 
 login_manager = LoginManager()
@@ -41,7 +36,7 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso!"})
@@ -63,7 +58,8 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password, role="user")
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role="user")
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuario cadastrado com sucesso"})
